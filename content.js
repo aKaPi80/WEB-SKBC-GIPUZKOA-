2f0e88a4c4b9c27d76a5ba70a5a45c705b402c32
aKaPi80 <alvarocalvo8@gmail.com>
2026-06-04 21:22:20 +0200

    Update SKBC Gipuzkoa website content

--- content.js
+++ content.js
@@ -471,7 +471,7 @@ window.SKBC_CONTENT = {
     ],
     "images": {
       "hero": "assets/people/equipo-tecnico.jpg",
-      "kids": "assets/people/uxue-garikano.jpg",
+      "kids": "assets/uploads/1780600913705-grupo-ninos.heic",
       "adults": "assets/people/alvaro-calvo.jpg",
       "learn": "assets/uploads/1780527476492-b9065214-9cd6-4696-b76a-066831c209fc.jpg",
       "gallery": [
