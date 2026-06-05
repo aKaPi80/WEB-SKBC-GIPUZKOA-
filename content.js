a9bc724be1b29d5be880449d7cfcfc6151fb8373
aKaPi80 <alvarocalvo8@gmail.com>
2026-06-05 14:41:24 +0200

    Update SKBC Gipuzkoa website content

--- content.js
+++ content.js
@@ -536,7 +536,7 @@ window.SKBC_CONTENT = {
     ],
     "testimonialInbox": {
       "enabled": true,
-      "supabaseUrl": "https://wucxazuhrgokvtajqmsr.supabase.co/rest/v1/",
+      "supabaseUrl": "https://wucxazuhrgokvtajqmsr.supabase.co",
       "anonKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1Y3hhenVocmdva3Z0YWpxbXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MjQzODQsImV4cCI6MjA5MzUwMDM4NH0.8Lg_0b7TSNEt69Uwv_1YQjJW5InFEZ3j3JXWQ1ZaYEU",
       "table": "skbc_testimonials"
     }
