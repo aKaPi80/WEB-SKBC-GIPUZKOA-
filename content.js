03d52e186f55361dac6c7e6b37ab6ac0a1c6ea22
aKaPi80 <alvarocalvo8@gmail.com>
2026-08-21 09:12:20 +0200

    Update SKBC Gipuzkoa website content

--- content.js
+++ content.js
@@ -53,7 +53,7 @@ window.SKBC_CONTENT = {
       },
       "url": "",
       "durationHours": "48",
-      "speedSeconds": "55",
+      "speedSeconds": "85",
       "expiresAt": "2026-08-31T00:00"
     },
     "specialVisual": {
